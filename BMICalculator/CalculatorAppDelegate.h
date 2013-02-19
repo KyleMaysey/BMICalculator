f17a520ba3916b11ac0a0a9fe8f405764ad93b3e
SaintD <TheGibson@SaintDs-Mac-Pro.local>
2013-02-18 19:15:42 -0800

    Initial Commit

---
+++ BMICalculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  BMICalculator
+//
+//  Created by SaintD on 2/18/13.
+//  Copyright (c) 2013 Kyle Maysey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BMICalculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  BMICalculator
+//
+//  Created by SaintD on 2/18/13.
+//  Copyright (c) 2013 Kyle Maysey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
