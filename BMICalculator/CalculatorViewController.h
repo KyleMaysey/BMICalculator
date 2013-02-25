7f12e3638433dfdc38b840241b1efb469459bb70
Kyle Maysey <Kyle_Maysey@csu.fullerton.edu>
2013-02-25 13:03:06 -0800

    Realized my previous push didn't update my controller view.  hopefully this will rectify the issue. Kyle Maysey.

--- BMICalculator/CalculatorViewController.h
+++ BMICalculator/CalculatorViewController.h
@@ -10,18 +10,22 @@
 
 @interface CalculatorViewController : UIViewController
 {
-    BOOL isMetric;
+    BOOL isMetric;  //Flag used to determine whether values are US customary or Metric
 }
 
-@property (weak, nonatomic) IBOutlet UITextField *heightValue;
-@property (weak, nonatomic) IBOutlet UITextField *weightValue;
-@property (weak, nonatomic) IBOutlet UILabel *heightUnit;
-@property (weak, nonatomic) IBOutlet UILabel *weightUnit;
-@property (weak, nonatomic) IBOutlet UILabel *bmiResult;
+@property (weak, nonatomic) IBOutlet UITextField *heightValue;  //Textbox for entering height
+@property (weak, nonatomic) IBOutlet UITextField *weightValue;  //Textbox for entering weight
+@property (weak, nonatomic) IBOutlet UILabel *heightUnit;       //Unit label of height
+@property (weak, nonatomic) IBOutlet UILabel *weightUnit;       //Unit label of weight
+@property (weak, nonatomic) IBOutlet UILabel *bmiResult;        //Label to show result of calculation
+@property (weak, nonatomic) IBOutlet UIImageView *imageResult;  //Outlet for image viewer
 
-
+//Purpose:  Action called by segment control.
+//          Toggles the units the calculation & the display labels of the units.
 -(IBAction)UnitToggle:(UISegmentedControl *)sender;
+
+//Purpose:  Action called by Calculate button.
+//          Performs the calculation for BMI, checks unit flag.  Does conversion on US customary units.
 -(IBAction)CalculateBMI:(UIButton *)sender;
--(IBAction)ClearKeyboard:(UIButton *)sender;
 
 @end
--- BMICalculator/CalculatorViewController.m
+++ BMICalculator/CalculatorViewController.m
@@ -14,9 +14,18 @@
 
 @implementation CalculatorViewController
 
-#define convertInchesToMetric .0254
-#define convertPoundsToMetric .453592
+#define convertInchesToMetric .0254     //Constant for conversion of inches to meters
+#define convertPoundsToMetric .453592   //Constant for conversion of pounds to kilograms
+#define _SeverlyThin 16.00              //Constant for determining "severly thin" range
+#define _ModeratelyThin 17.00           //Constant for determining "moderately thin" range
+#define _MildlyThin 18.5                //Constant for determining "mildy thin" range
+#define _Normal 25.00                   //Constant for determining "normal" range
+#define _Overweight 30.00               //Constant for determining "overweight" range
+#define _ModerateObesity 35.00          //Constant for determining "moderately obese" range
+#define _SeverObesity 40.00             //Constant for determining "severly obese" range
 
+//Purpose:  Action called by segment control.
+//          Toggles the units the calculation & the display labels of the units.
 -(IBAction)UnitToggle:(UISegmentedControl *)sender
 {
     //NSLog(@"Selected index: %d", [sender selectedSegmentIndex]);
@@ -39,16 +48,13 @@
     }
 }
 
--(IBAction)ClearKeyboard:(UIButton *)sender
-{
-    [self.heightValue resignFirstResponder];
-    [self.weightValue resignFirstResponder];
-}
-
+//Purpose:  Action called by Calculate button.
+//          Performs the calculation for BMI, checks unit flag.  Does conversion on US customary units.
 -(IBAction)CalculateBMI:(UIButton *)sender
 {
-    double BMIVal;
+    double BMIVal;      //Temporary value to store BMI calculation result
     
+    //Perform calculation, converts units if US customary measurements
     if (isMetric)
     {
         BMIVal = [self.weightValue.text doubleValue]/([self.heightValue.text doubleValue] * [self.heightValue.text doubleValue] * .0001);
@@ -58,9 +64,56 @@
         BMIVal = ([self.weightValue.text doubleValue] * convertPoundsToMetric) / ([self.heightValue.text doubleValue] * convertInchesToMetric * [self.heightValue.text doubleValue] * convertInchesToMetric);
     }
     
-    self.bmiResult.text = [NSString stringWithFormat:@"%f", BMIVal];
+    self.bmiResult.text = [NSString stringWithFormat:@"%f", BMIVal];        //Set result labels text
+    
+    NSBundle* myBundle	= [NSBundle mainBundle];                            //Create bundle for files
+    NSString* imagePath;
+    
+    //Finds path of image file for BMI category
+    if (BMIVal < _SeverlyThin)
+    {
+        imagePath = [myBundle pathForResource:@"SevereThin" ofType:@"png"];
+    }
+    else if (BMIVal < _ModeratelyThin)
+    {
+        imagePath = [myBundle pathForResource:@"ModThin" ofType:@"png"];
+    }
+    else if (BMIVal < _MildlyThin)
+    {
+        imagePath = [myBundle pathForResource:@"MildThin" ofType:@"png"];
+    }
+    else if (BMIVal < _Normal)
+    {
+        imagePath = [myBundle pathForResource:@"Normal" ofType:@"png"];
+    }
+    else if (BMIVal < _Overweight)
+    {
+        imagePath = [myBundle pathForResource:@"Over" ofType:@"png"];
+    }
+    else if (BMIVal < _ModerateObesity)
+    {
+        imagePath = [myBundle pathForResource:@"ModObes" ofType:@"png"];
+    }
+    else if (BMIVal < _SeverObesity)
+    {
+        imagePath = [myBundle pathForResource:@"SevObes" ofType:@"png"];
+    }
+    else
+    {
+        imagePath = [myBundle pathForResource:@"VSevObes" ofType:@"png"];
+    }
+    
+    self.imageResult.image = [UIImage imageWithContentsOfFile:imagePath];   //Set image
     
     //NSLog(@"BMIValue: %f", BMIVal);
+}
+
+
+//Purpose:  Remove numerical keypad when user touches main view
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.heightValue resignFirstResponder];
+    [self.weightValue resignFirstResponder];
 }
 
 - (void)viewDidLoad
